61d6477c7c8875d60f7f36cceb8b427d767442b7
Richard Pittman <rdpgamedev@gmail.com>
2020-02-05 13:57:06 -0800

    Initial commit

---
+++ Assets/Scripts/ProjectButton.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ProjectButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public float maxScale = 2f;
+    public float transitionLength = 0.5f;
+
+    Vector3 baseScale;
+    Vector3 endScale;
+    float tScale = 0f;
+
+    public void Start()
+    {
+        baseScale = transform.localScale;
+        endScale = baseScale * maxScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(ScaleUp());
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StopAllCoroutines();
+        StartCoroutine(ScaleDown());
+    }
+
+    IEnumerator ScaleUp ()
+    {
+        if (tScale < 0.0f) tScale = 0.0f;
+        float curTime = tScale * transitionLength;
+
+        while (tScale < 1.0f)
+        {
+            transform.localScale = Vector3.Lerp(baseScale, endScale, tScale);
+            curTime += Time.deltaTime;
+            tScale = curTime / transitionLength;
+            yield return null;
+        }
+        transform.localScale = baseScale * maxScale;
+        yield return null;
+    }
+
+    IEnumerator ScaleDown ()
+    {
+        if (tScale > 1.0f) tScale = 1.0f;
+        float curTime = tScale * transitionLength;
+
+        while (tScale > 0f)
+        {
+            transform.localScale = Vector3.Lerp(baseScale, endScale, tScale);
+            curTime -= Time.deltaTime;
+            tScale = curTime / transitionLength;
+            yield return null;
+        }
+        transform.localScale = baseScale;
+        yield return null;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Unity-Prototype-Portfolio
