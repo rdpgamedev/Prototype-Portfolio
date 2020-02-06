1c2712f705d452a425ed021a3cabab3f4eed709d
Richard Pittman <rdpgamedev@gmail.com>
2020-02-06 13:23:04 -0800

    Change ProjectDescription
    Adds the use of a CanvasGroup to the ProjectDescription to modify the alpha of its panel and its child TextMeshPro.

--- Assets/Scripts/ProjectDescription.cs
+++ Assets/Scripts/ProjectDescription.cs
@@ -9,29 +9,15 @@ public class ProjectDescription : MonoBehaviour
     public float outY = 150f;
     public float inY = 90f;
 
-    Image panel;
-    TextMeshProUGUI text;
+    CanvasGroup canvasGroup;
 
     float curTime;
     Vector3 outPos;
     Vector3 inPos;
 
-    Color panelColor;
-    Color32 textColor;
     void Awake()
     {
-        panel = GetComponent<Image>();
-        panelColor = panel.color;
-        text = GetComponentInChildren<TextMeshProUGUI>();
-        textColor = text.color;
-
-        Color tempColor = panelColor;
-        tempColor.a = 0f;
-        panel.color = tempColor;
-
-        Color tempColor32 = textColor;
-        tempColor32.a = 0;
-        text.color = tempColor32;
+        canvasGroup = GetComponent<CanvasGroup>();
     }
 
     void Start()
@@ -42,17 +28,11 @@ public class ProjectDescription : MonoBehaviour
 
     public IEnumerator SlideIn(float duration)
     {
-        Color tempColor = panel.color;
-        Color32 tempTextColor = text.color;
         while (curTime < 1.0f)
         {
             this.transform.localPosition = Vector3.Lerp(outPos, inPos, curTime);
 
-            tempColor.a = curTime * curTime * panelColor.a;
-            panel.color = tempColor;
-
-            tempTextColor.a = (byte)(curTime * curTime * textColor.a);
-            text.color = tempTextColor;
+            canvasGroup.alpha = curTime;
 
             curTime += (Time.deltaTime / duration);
             yield return null;
@@ -61,35 +41,24 @@ public class ProjectDescription : MonoBehaviour
 
         this.transform.localPosition = inPos;
 
-        panel.color = panelColor;
-
-        text.color = textColor;
+        canvasGroup.alpha = curTime;
     }
 
     public IEnumerator SlideOut(float duration)
     {
-        Color tempColor = panel.color;
-        Color32 tempTextColor = text.color;
         while (curTime > 0.0f)
         {
             this.transform.localPosition = Vector3.Lerp(outPos, inPos, curTime);
 
-            tempColor.a = curTime * curTime * panelColor.a;
-            panel.color = tempColor;
-
-            tempTextColor.a = (byte)(curTime * curTime * textColor.a);
-            text.color = tempTextColor;
+            canvasGroup.alpha = curTime;
 
             curTime -= (Time.deltaTime / duration);
             yield return null;
         }
         curTime = 0.0f;
+        
         this.transform.localPosition = outPos;
 
-        tempColor.a = 0.0f;
-        panel.color = tempColor;
-
-        tempTextColor.a = 0;
-        text.color = tempTextColor;
+        canvasGroup.alpha = curTime;
     }
 }
